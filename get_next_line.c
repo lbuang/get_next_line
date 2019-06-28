397cfa892993ee87f5b246dc95fa429a8af9a600
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-28 14:07:37 +0200

    first commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,38 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/26 10:22:20 by lbuang            #+#    #+#             */
-/*   Updated: 2019/06/26 10:24:06 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/28 13:54:05 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
+#include "get_next_line.h"
 
+int	get_next_line(const int fd, char **line)
+{
+	static char	*c[2147483647];
+	char		buffer[BUFF_SIZE + 1];
+	char		*tmp;
+	ssize_t		b;
+	int			endl;
+
+	if (fd < 0 || (!c[fd] && !(c[fd] = ft_strnew(1))) || !line)
+		return (-1);
+	while (!ft_strchr(c[fd], '\n') && (b = read(fd, buffer, BUFF_SIZE)) > 0)
+	{
+		buffer[b] = '\0';
+		tmp = c[fd];
+		c[fd] = ft_strjoin(c[fd], buffer);
+		ft_strdel(&tmp);
+	}
+	if (b == -1 || !*(tmp = c[fd]))
+		return (b == -1 ? -1 : 0);
+	if ((endl = (ft_strchr(c[fd], '\n') > 0)))
+		*line = ft_strsub(c[fd], 0, ft_strchr(c[fd], '\n') - c[fd]);
+	else
+		*line = ft_strdup(c[fd]);
+	c[fd] = ft_strsub(c[fd], (unsigned int)(ft_strlen(*line) + endl),
+			(size_t)(ft_strlen(c[fd]) - (ft_strlen(*line) + endl)));
+	ft_strdel(&tmp);
+	return (!(!c[fd] && !ft_strlen(*line)));
+}
---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/28 13:50:45 by lbuang            #+#    #+#             */
+/*   Updated: 2019/06/28 14:05:58 by lbuang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# include <unistd.h>
+
+int					get_next_line(const int fd, char **line);
+
+#endif
